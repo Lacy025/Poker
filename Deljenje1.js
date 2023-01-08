f0f44ab3dfd97b23605155119d1905d15df2c61e
Lacy025 <bacilacika@yahoo.com>
2023-01-08 14:40:59 +0100

    1.0.3

--- Deljenje1.js
+++ Deljenje1.js
@@ -4,6 +4,12 @@ let $k3;
 let $k4;
 let $k5;
 
+let $n1;
+let $n2;
+let $n3;
+let $n4;
+let $n5;
+
 let $b1;
 let $b2;
 let $b3;
@@ -11,36 +17,36 @@ let $b4;
 let $b5;
 
 if($deljenje==0) {
-    timerCekanje = setInterval(cekanje, 100);
-}    
-function cekanje() {
-    console.log($deljenje);
 
-    if($deljenje==1) {
-        clearInterval(timerCekanje);
-        prvoDeljenje();
-        console.log($k1);
-        console.log($k2);
-        console.log($k3);
-        console.log($k4);
-        console.log($k5);
+    if($deljenje==0) {
+        timerCekanje1 = setInterval(cekanje, 500);
+    }    
+    function cekanje() {
+        console.log($deljenje);
+    
+        if($deljenje==1) {
+            clearInterval(timerCekanje1);
+            prvoDeljenje();
+            console.log($k1);
+            console.log($k2);
+            console.log($k3);
+            console.log($k4);
+            console.log($k5);
+            $deljenje = 2;
+        }
+    
+    }
+    function prvoDeljenje() {
+    
+        while($k1==$k2||$k1==$k3||$k1==$k4||$k1==$k5||$k2==$k3||
+            $k2==$k4||$k2==$k5||$k3==$k4||$k3==$k5||$k4==$k5) {
+            $k1 = Math.floor(Math.random() * 52);
+            $k2 = Math.floor(Math.random() * 52);
+            $k3 = Math.floor(Math.random() * 52);
+            $k4 = Math.floor(Math.random() * 52);
+            $k5 = Math.floor(Math.random() * 52);
+        }
+    
     }
 
-}
-function prvoDeljenje() {
-
-    while($k1==$k2||$k1==$k3||$k1==$k4||$k1==$k5||$k2==$k3||
-        $k2==$k4||$k2==$k5||$k3==$k4||$k3==$k5||$k4==$k5) {
-        $k1 = Math.floor(Math.random() * 52);
-        $k2 = Math.floor(Math.random() * 52);
-        $k3 = Math.floor(Math.random() * 52);
-        $k4 = Math.floor(Math.random() * 52);
-        $k5 = Math.floor(Math.random() * 52);
-    }
-
-}
-function Karta1() {
-    if($k1==0) {
-        
-    }
 }
--- Index.js
+++ Index.js
@@ -15,3 +15,23 @@ var $d6;
 var $d7;
 var $d8;
 var $d9;
+
+$polje1.style.position = "absolute";
+$polje1.style.left = "10px";
+$polje1.style.top = "320px";
+
+$polje2.style.position = "absolute";
+$polje2.style.left = "390px";
+$polje2.style.top = "320px";
+
+$polje3.style.position = "absolute";
+$polje3.style.left = "770px";
+$polje3.style.top = "320px";
+
+$polje4.style.position = "absolute";
+$polje4.style.left = "1150px";
+$polje4.style.top = "320px";
+
+$polje5.style.position = "absolute";
+$polje5.style.left = "1530px";
+$polje5.style.top = "320px";
