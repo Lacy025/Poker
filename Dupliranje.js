0395d1a57c80bdab5cf1c8a32fa354246e08f3d5
Lacy025 <bacilacika@yahoo.com>
2023-01-24 00:31:14 +0100

    1.3.6

--- Dupliranje.js
+++ Dupliranje.js
@@ -1,18 +1,35 @@
-if($deljenje==0) {
-    timerCekanje9 = setInterval(cekanje, 500);
-}    
-function cekanje() {
+$timerCekanje9 = setInterval(Cekanje9, 500);
+ 
+function Cekanje9() {
     console.log($deljenje);
 
     if($deljenje==10) {
-        clearInterval(timerCekanje9);
+        $deljenje = 12;
+        clearInterval($timerCekanje9);
+        $polje01.style.visibility = "visible";
+        veca.style.visibility = "visible";
+        document.getElementById("audioVeca").play();
+        audioCount1.currentTime = 0;
+        $timerManja = setInterval(Manja, 400);
+        $timerVeca = setInterval(Veca, 800);
+        
+        veca.style.visibility = "visible";
         
     }
-    else {
-        clearInterval(timerCekanje9);
+}   function Veca() {
+        veca.style.visibility = "visible";
+        manja.style.visibility = "hidden";
+        audioManja.currentTime = 2;
+        document.getElementById("audioVeca").play();
+        audioVeca.currentTime = 0;
     }
-
-}
+    function Manja() {
+        veca.style.visibility = "hidden";
+        manja.style.visibility = "visible";
+        audioVeca.currentTime = 2;
+        document.getElementById("audioManja").play();
+        audioManja.currentTime = 0;
+    }
    
    
 
